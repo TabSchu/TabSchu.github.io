848d6d4f595d00ef7fc984a73b3cb8b0ce46aa0e
TabSchu <t.drieschner@me.com>
2020-05-17 20:21:34 +0200

    create react setup

---
+++ src/HeaderBar.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class HeaderBar extends Component {
+
+    render() {
+        return(
+            <div id="headerBar">
+
+               <h1 id="wortmarke">zphere</h1>
+                <a id="filter">Filter</a>
+
+            </div>
+        )
+    }
+}
+
+export default HeaderBar;
