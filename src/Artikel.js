033c4bc0ef8da55167ab8d066a2d35e0fa51ab37
ezevkyx <ezevkyx@gmx.de>
2020-06-14 00:49:27 +0200

    Artikel update

--- src/Artikel.js
+++ src/Artikel.js
@@ -43,26 +43,27 @@ class Artikel extends Component {
                 <div id="titel">
                     <h5>Breaking News:</h5>
                     <h3>Neuer Chef im Verein - <br></br>
-Jonny übernimmt!</h3>
+                        Jonny übernimmt!</h3>
                 </div>
-                <div id="autor">ICON Rainald Becker</div>
+                <div id="autor">ICON Rainald Becker<div className="icon">ICON </div></div>
                 <div id="text">In Bremen geht es wieder einmal rund. Auf dem Tanzparkett präsentieren sich die besten Tanzformationen der Welt in der Kategorie Latein und tanzen um den Titel des Weltmeisters.
                     <br></br>
                     Die Lateinformation des Bremer Grün Gold Clubs ist international das Maß aller Dinge: sieben Mal Weltmeister, zuletzt vier Mal in Folge. Auch in diesem Jahr gilt der Titelverteidiger mit Heimvorteil als Favorit.
                     Für Spannung ist dennoch gesorgt, denn die Konkurrenz brennt darauf, endlich einmal diese Bremer Formation zu schlagen – ein Tanzabend im Hochglanzformat.</div>
                 <div id="info">
-                    <div id="hashtag"></div>
+                    <div id="hashtag">#Meister #tanzen</div>
                     <h3>Generelle Infos</h3>
                     <ul>
-                        <li></li>
+                        <li>1. Deutsche Liga</li>
+                        <li>Grün Gold Club Bremen</li>
                     </ul>
                 </div>
                 <h3>Ähnliche Beiträge: </h3>
                 <div className="stories">
                     <div className="story"><div className="boxcontent"><h3>story</h3><h4>David</h4></div></div> 
+                    {/* <div className="story"><div className="boxcontent"><h3>story</h3><h4>David</h4></div></div> 
                     <div className="story"><div className="boxcontent"><h3>story</h3><h4>David</h4></div></div> 
-                    <div className="story"><div className="boxcontent"><h3>story</h3><h4>David</h4></div></div> 
-                    <div className="story"><div className="boxcontent"><h3>story</h3><h4>David</h4></div></div>                 
+                    <div className="story"><div className="boxcontent"><h3>story</h3><h4>David</h4></div></div>                  */}
                 </div>
             </div>
             <Switch> 
