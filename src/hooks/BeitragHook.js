55397bcff24fa975a4c6f751ea71fe0b1b45843d
TabSchu <t.drieschner@me.com>
2020-06-23 09:51:15 +0200

    BeitragsHook: check if podcast, to fix Newsfeed Podcasts

--- src/hooks/BeitragHook.js
+++ src/hooks/BeitragHook.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useFetch } from "../hooks";
 import Beitrag from "../Beitrag";
+import BeitragPodcast from "../BeitragPodcast";
 function BeitragHook(props) {
     const [data, loading] = useFetch(
         props.fetch_url
@@ -15,14 +16,28 @@ function BeitragHook(props) {
                 <div>
                     {/* <div className="bigBlock">
                         <div className="bigBox"> */}
-                            {data.map(({ id_beitrag, titel, img_url, kategorie, teaser, typ, sportart, sport }) => (
-                                <div key={`beitrag_${id_beitrag}`}>
-                                    <Beitrag isMerkliste={props.isMerkliste}
-                                        id_beitrag={id_beitrag}  titel={titel}   img_url={img_url}  teaser={teaser}
-                                        kategorie={kategorie}  medientyp={typ}   sport_id={sport}  sportart={sportart}
-                                    />
-                                </div>
-                            ))}
+                            {data.map(({ id_beitrag, titel, img_url, kategorie, teaser, typ, sportart, sport }) => {//(
+                                if(typ==="Podcast"){
+                                    return(
+                                            <div key={`beitrag_${id_beitrag}`}>
+                                                <BeitragPodcast
+                                                    id_beitrag={id_beitrag}  titel={titel} sport_id={sport}  sportart={sportart}
+                                                />
+                                            </div>
+                                          )
+                                }else {
+                                    return (
+                                        <div key={`beitrag_${id_beitrag}`}>
+                                            <Beitrag isMerkliste={props.isMerkliste}
+                                                id_beitrag={id_beitrag}  titel={titel}   img_url={img_url}  teaser={teaser}
+                                                kategorie={kategorie}  medientyp={typ}   sport_id={sport}  sportart={sportart}
+                                                />
+                                        </div>
+                                    )
+                                }
+                            }
+                            //)
+                                )}
                         {/* </div>
                     </div> */}
 
